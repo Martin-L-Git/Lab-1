1d303f6a63ae33946bcbc320678b41cff676efa8
Martin Lidgren <martin.lidgren@live.se>
2022-11-25 15:01:00 +0100

    Update Volvo240.java

--- src/Volvo240.java
+++ src/Volvo240.java
@@ -1,19 +1,18 @@
 package src;
+
 import java.awt.*;
 
-public class Volvo240 extends Car{
+public class Volvo240 extends Car {
 
     private final static double trimFactor = 1.25;
 
-    
-    public Volvo240 () {
+    public Volvo240() {
         super(4, Color.black, 100, "Volvo240");
     }
 
     @Override
-    public double speedFactor(){
+    public double speedFactor() {
         return getEnginePower() * 0.01 * trimFactor;
     }
 
 }
-
